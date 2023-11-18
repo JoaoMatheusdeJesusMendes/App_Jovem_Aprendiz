08fe392a0db748836bcaeff6a5c9bd722d20b045
Joao <jm2matheus@gamil.com>
2023-11-18 18:42:03 -0300

    Componente

--- Front-end/Cadastro.js
+++ Front-end/Cadastro.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, ImageBackground} from 'react-native';
+import React, {useState} from 'react'
+import logo from "./assets/LogoItemm.png";
 
 function Cadastro({navigation}){
   const [nome, setNome] = useState("");
